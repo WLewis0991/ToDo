4e767dc245266c8b4ec8ea87d1a85385a8d200be
wlewis0991 <wlewis1990@gmail.com>
2026-01-31 10:33:55 -0800

    Modal added to add project but not functional

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+ const addProjectButton = document.getElementById("addProjectButton");
+ const projectModal = document.getElementById("addProjectModal");
+ addProjectButton.addEventListener("click", () => {
+    projectModal.showModal();
+ })
