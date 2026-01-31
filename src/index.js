e493ae1ec6e58404481cd79f98157510c0350f5c
wlewis0991 <wlewis1990@gmail.com>
2026-01-31 11:53:29 -0800

    Add project modal moved to seperate js file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 // src/index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import { openProjectModal } from "./addProject.js";
 
 console.log(greeting);
 
+const addProjectButton = document.getElementById("addProjectButton");
 
-
- const addProjectButton = document.getElementById("addProjectButton");
- const projectModal = document.getElementById("addProjectModal");
- const addProject = document.getElementById("addProject");
- const cancelProject = document.getElementById("cancelProject");
- const projectNameInput = document.getElementById("projectNameInput");
-
- addProjectButton.addEventListener("click", () => {
-    projectModal.showModal();
- })
-
- addProject.addEventListener("click", () => {
-    projectModal.close();
- })
-
- cancelProject.addEventListener("click", () => {
-    projectNameInput.value="";
-    projectModal.close();
- })
+addProjectButton.addEventListener("click", openProjectModal);
