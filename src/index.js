945a84385a56c6d575bef1ad85db2d042782c486
wlewis0991 <wlewis1990@gmail.com>
2026-01-31 11:25:54 -0800

    Add project modal styles but not functional

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "./styles.css";
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
+
+
+
  const addProjectButton = document.getElementById("addProjectButton");
  const projectModal = document.getElementById("addProjectModal");
  addProjectButton.addEventListener("click", () => {
