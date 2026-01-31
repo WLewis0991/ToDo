938c65e520731898ad340a178b46dc03bfedfefe
wlewis0991 <wlewis1990@gmail.com>
2026-01-31 11:41:16 -0800

    Add project modal buttons now close the modal but dont send data

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ console.log(greeting);
 
  const addProjectButton = document.getElementById("addProjectButton");
  const projectModal = document.getElementById("addProjectModal");
+ const addProject = document.getElementById("addProject");
+ const cancelProject = document.getElementById("cancelProject");
+ const projectNameInput = document.getElementById("projectNameInput");
+
  addProjectButton.addEventListener("click", () => {
     projectModal.showModal();
+ })
+
+ addProject.addEventListener("click", () => {
+    projectModal.close();
+ })
+
+ cancelProject.addEventListener("click", () => {
+    projectNameInput.value="";
+    projectModal.close();
  })
