2acdf4778169e39fc0a24efbfdaea84e2fd1d356
wlewis0991 <wlewis1990@gmail.com>
2026-02-02 13:09:50 -0800

    Unique ID added to be able to access each object saved to local storage

--- src/addProject.js
+++ src/addProject.js
@@ -3,6 +3,7 @@ import { projects } from "./storage.js";
 export function AddNewProject (name) {
     this.name = name;
     this.tasks = [];
+    this.id = crypto.randomUUID();
 
     projects.push(this);
     localStorage.setItem("projects", JSON.stringify(projects))
