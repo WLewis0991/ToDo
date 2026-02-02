c041a2324e7f15d7386b99236f33236d4f81dbde
wlewis0991 <wlewis1990@gmail.com>
2026-02-02 10:15:46 -0800

    Fuctionally to add project to Local Storage added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 // src/index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
 import { openProjectModal } from "./addProject.js";
-
-console.log(greeting);
 
 const addProjectButton = document.getElementById("addProjectButton");
 
 addProjectButton.addEventListener("click", openProjectModal);
+
+const projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+function AddNewProject (name) {
+    this.name = name;
+    this.tasks = [];
+
+    projects.push(this);
+    localStorage.setItem("projects", JSON.stringify(projects))
+}
+
+const projectNameInput = document.getElementById("projectNameInput");
+const addProject = document.getElementById("addProject");
+addProject.addEventListener("click", () => {
+    let newProject = projectNameInput.value;
+    newProject = new AddNewProject(newProject)
+
+})
