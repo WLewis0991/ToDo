6608c7865cde677a27c15e62092fb72e443a328e
wlewis0991 <wlewis1990@gmail.com>
2026-02-02 10:59:53 -0800

    Project names stored to storage displayed in project list

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@
 import "./styles.css";
 import { openProjectModal } from "./openProjectModule.js";
 import { AddNewProject } from "./addProject.js";
+import { projects } from "./storage.js";
+
 
 const addProjectButton = document.getElementById("addProjectButton");
 
 addProjectButton.addEventListener("click", openProjectModal);
-
-const projects = JSON.parse(localStorage.getItem("projects")) || [];
-
 
 const projectNameInput = document.getElementById("projectNameInput");
 const addProject = document.getElementById("addProject");
 addProject.addEventListener("click", () => {
     let newProject = projectNameInput.value;
     newProject = new AddNewProject(newProject)
+})
+
+projects.forEach (project => {
+    const projectList = document.getElementById("projectList");
+    const projectItem = document.createElement("li");
+    projectItem.innerText = project.name;
+    projectList.appendChild(projectItem);
 
 })
