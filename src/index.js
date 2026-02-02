3ca444922b828e970904e7124fdf62d52c91ae24
wlewis0991 <wlewis1990@gmail.com>
2026-02-02 12:38:30 -0800

    Ability to add tasks and have them displayed added

--- src/index.js
+++ src/index.js
@@ -35,3 +35,51 @@ addTaskButton.addEventListener("click", () => {
 cancelTask.addEventListener("click", () => {
     addTaskModal.close();
 })
+
+// task display test
+const tasklist = document.getElementById("taskList");
+const addNewTask = document.getElementById("addTask");
+
+addNewTask.addEventListener("click", () => {
+    const taskName = document.getElementById("taskNameInput").value;
+    const taskDescription = document.getElementById("taskDescriptionInput").value;
+    const taskDueDate = document.getElementById("taskDueDateInput").value;
+    const priorityOptions = document.getElementsByName("priority");
+    let taskPriority = "Low"; // Default priority
+    for (const option of priorityOptions) {
+        if (option.checked) {
+            taskPriority = option.value;
+            break;
+        }   
+    }
+
+    const taskDiv = document.createElement("div");
+    taskDiv.id = "task";
+
+    const taskNameDiv = document.createElement("div");
+    taskNameDiv.innerText = taskName;
+
+    const taskDescriptionDiv = document.createElement("div");
+    taskDescriptionDiv.innerText = taskDescription;
+
+    const taskDueDateDiv = document.createElement("div");
+    taskDueDateDiv.innerText = `Due Date: ${taskDueDate}`;
+
+    const taskPriorityDiv = document.createElement("div");
+    taskPriorityDiv.innerText = `Priority: ${taskPriority}`;
+
+    taskDiv.appendChild(taskNameDiv);
+    taskDiv.appendChild(taskDescriptionDiv);
+    taskDiv.appendChild(taskDueDateDiv);
+    taskDiv.appendChild(taskPriorityDiv);
+
+    tasklist.appendChild(taskDiv);
+
+    taskNameInput.value = "";
+    taskDescriptionInput.value = "";
+    taskDueDateInput.value = "";
+    priorityOptions.forEach(option => option.checked = false);
+
+    addTaskModal.close();
+
+})
