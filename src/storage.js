ba33e23cd5fab69fa5329b405a896c7910e9e3e6
wlewis0991 <wlewis1990@gmail.com>
2026-02-04 10:10:51 -0800

    Default added with update to first time opening

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,30 @@
 //Storage of Projects and the tasks tied to them
 
 export const projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+if (projects.length === 0) {
+    projects.push({
+        id: crypto.randomUUID(),
+        name: "Getting Started",
+        tasks: [
+            {
+                id: crypto.randomUUID(),
+                title: "Welcome to your To-Do App",
+                description: "This is a sample task",
+                dueDate: "",
+                priority: "Low",
+                
+            },
+            {
+                id: crypto.randomUUID(),
+                title: "Add your first task",
+                description: "Click the Add Task button",
+                dueDate: "",
+                priority: "Medium",
+               
+            }
+        ]
+    });
+
+    localStorage.setItem("projects", JSON.stringify(projects));
+}
